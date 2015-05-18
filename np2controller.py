bce9ae45054b20e883f75c18d55284e9d6fe2a52
Andreas Mauer <andalmansa@gmail.com>
2015-05-18 17:22:45 +0200

    Update np2controller.py

--- np2controller.py
+++ np2controller.py
@@ -14,6 +14,8 @@ class Controller:
         self.User = np2user.Input()
         self.Report = np2report.Report()
         self.Crawler = np2crawler.Crawler()
+        self.Brands = np2config.variables['brands']
+       
 
         # then route the control to the reports
         self.router()
@@ -65,16 +67,77 @@ class Controller:
         self.Report.print_dictionary(self.User.get('reporttype'), np2config.variables['kpis'],
             self.User.get('weeks'), self.Crawler.kpis)
 
+
     def categories_report(self):
 
+        self.brand = ['spex', 'mister']
+
+
+        glassesPositives = {
+            'de': ['brille'],
+            'ch': ['brille'],
+            'es': ['gafa'],
+            'fr': ['lunette'],
+            'uk': ['glasses'],
+            'nl': ['brile'],
+        }
+
+        sunglassesPositives = {
+            'de': ['sonne'],
+            'ch': ['sonne'],
+            'es': ['sol'],
+            'fr': ['sol'],
+            'uk': ['sun'],
+            'nl': ['zonnen'],
+        }
+
+        lensesPositives = {
+            'de': ['linse', 'kontakt'],
+            'ch': ['linse', 'kontakt'],
+            'es': ['lentilla', 'lentes', 'contacto'],
+            'fr': ['lentille', 'contact'],
+            'uk': ['contact', 'lense'],
+            'nl': ['kontakt'],
+        }
+
+        self.glassesPositives = glassesPositives[self.User.get('country')]
+        self.glassesNegatives = self.brand[:]
+        self.glassesNegatives.extend(sunglassesPositives[self.User.get('country')])
+        self.sunglassesPositives = sunglassesPositives[self.User.get('country')]
+        self.sunglassesNegatives = self.brand[:]
+        self.lensesPositives = lensesPositives[self.User.get('country')]
+        self.lensesNegatives = self.brand[:]
+        self.lensesNegatives.extend(sunglassesPositives[self.User.get('country')])
+        self.lensesNegatives.extend(glassesPositives[self.User.get('country')])
+        self.brandsPositives = self.Brands
+        self.brandsNegatives = self.brand[:]
+        self.brandsNegatives.extend(sunglassesPositives[self.User.get('country')])
+        self.brandsNegatives.extend(glassesPositives[self.User.get('country')])
+        self.brandsNegatives.extend(lensesPositives[self.User.get('country')])
+        self.othersPositives = ['']
+        self.othersNegatives = self.brandsNegatives[:]
+        self.othersNegatives.extend(self.brandsPositives)
 
 
         # crawl the csvs
         self.Crawler.crawl(self.User.get('path'), self.User.get('weeks'),
-            ['spex', 'mister'], ['negative values here'], 'consolidated_init', 'brand')
+            self.brand, ['negative values here'], 'consolidated_init', 'brand')
 
         self.Crawler.crawl(self.User.get('path'), self.User.get('weeks'),
-            ['brille'], ['sonne'], 'consolidated_init', 'brille')
+            self.glassesPositives, self.glassesNegatives, 'consolidated_init', 'brille')
+
+        self.Crawler.crawl(self.User.get('path'), self.User.get('weeks'),
+            self.sunglassesPositives, self.sunglassesNegatives, 'consolidated_init', 'sonne')
+
+        self.Crawler.crawl(self.User.get('path'), self.User.get('weeks'),
+            self.lensesPositives, self.lensesNegatives, 'consolidated_init', 'linse')
+
+        self.Crawler.crawl(self.User.get('path'), self.User.get('weeks'),
+            self.brandsPositives, self.brandsNegatives, 'consolidated_init', 'brands')
+
+        self.Crawler.crawl(self.User.get('path'), self.User.get('weeks'),
+            self.othersPositives, self.othersNegatives, 'consolidated_init', 'others')
+
         
         # print in report
         self.Report.restore(self.User.get('path'), self.User.get('filename_of_report'))
@@ -117,13 +180,3 @@ class Controller:
 
 
 a = Controller()
-
-
-
-
-
-
-
-
-
-
