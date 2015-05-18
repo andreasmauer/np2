f2768731b5360de807c1de94d1e1b5b082a03ba1
Andreas Mauer <andalmansa@gmail.com>
2015-05-18 17:22:17 +0200

    Update np2config.py

--- np2config.py
+++ np2config.py
@@ -6,7 +6,8 @@ import sys
 variables = {
     
     'keyword_categories': ['brille', 'sonne', 'linsen', 'brands', 'others', 'brand'],
-    'reports': ['single keyword', 'multiple keywords', 'full category', 'categories', 'regex'],
+    'brands': ['adidas', 'arnette', 'blue bay', 'boss', 'brendel', 'burberry', 'carrera', 'chiemsee', 'ck', 'calvin', 'diesel', 'dkny', 'dolce', 'gabanna', 'd&g', 'dsquared', 'emporio', 'armani', 'escada', 'esprit', 'fossil', 'furla', 'gant', 'gucci', 'guess', 'hackett', 'humphreys', 'italia', 'jaguar', 'jee vice', 'jette joop', 'jil sander', 'joop', 'cavalli', 'karen', 'lacoste', 'le specs', 'loveyewear', 'lozza', 'jacobs', 'polo', 'mara', 'mexx', 'kors', 'michalsky', 'nike', 'nozomi', 'oakley', 'oxydo', 'persol', 'police', 'ralph', 'lauren', 'porsche', 'prada', 'puma', 'quicksilver', 'ray', 'ban', 'replay', 'rosy', 'belluni', 'scott', 'superdry', 'timberland', 'titanflex', 'ford', 'hilfiger', 'uvex', 'vogue', 'wood fellas', 'xray'],
+    'reports': ['single keyword', 'multiple keywords', 'full category', 'categories', 'regex', 'test'],
     'path': '',
     'kpis': ('clicks', 'impressions', 'ranking', 'ctr'),
     'os': '', 
@@ -21,3 +22,4 @@ else:
 
     variables['path'] = r'R:\Marketing\Channels\SEO\Ongoing\Regularly - Weekly reports\python' + '\\'
     variables['os'] = 'windows'
+
