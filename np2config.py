ac32d2fa047f39555b28e6295540de5e9df9feca
Andreas Mauer <andalmansa@gmail.com>
2015-06-15 17:01:42 +0200

    adding new reports

--- np2config.py
+++ np2config.py
@@ -7,10 +7,36 @@ variables = {
     
     'keyword_categories': ['brille', 'sonne', 'linsen', 'brands', 'others', 'brand'],
     'brands': ['adidas', 'arnette', 'blue bay', 'boss', 'brendel', 'burberry', 'carrera', 'chiemsee', 'ck', 'calvin', 'diesel', 'dkny', 'dolce', 'gabanna', 'd&g', 'dsquared', 'emporio', 'armani', 'escada', 'esprit', 'fossil', 'furla', 'gant', 'gucci', 'guess', 'hackett', 'humphreys', 'italia', 'jaguar', 'jee vice', 'jette joop', 'jil sander', 'joop', 'cavalli', 'karen', 'lacoste', 'le specs', 'loveyewear', 'lozza', 'jacobs', 'polo', 'mara', 'mexx', 'kors', 'michalsky', 'nike', 'nozomi', 'oakley', 'oxydo', 'persol', 'police', 'ralph', 'lauren', 'porsche', 'prada', 'puma', 'quicksilver', 'ray', 'ban', 'replay', 'rosy', 'belluni', 'scott', 'superdry', 'timberland', 'titanflex', 'ford', 'hilfiger', 'uvex', 'vogue', 'wood fellas', 'xray'],
-    'reports': ['single keyword', 'multiple keywords', 'full category', 'categories', 'regex', 'test'],
+    'reports': ['single keyword', 'multiple keywords', 'categories', 'regex', 'test', 'brille', 'sonne', 'linse', 'brands', 'brand', 'others'],
     'path': '',
     'kpis': ('clicks', 'impressions', 'ranking', 'ctr'),
     'os': '', 
+    'glassesPositives': {
+            'de': ['brille'],
+            'ch': ['brille'],
+            'es': ['gafa'],
+            'fr': ['lunette'],
+            'uk': ['glasses'],
+            'nl': ['brile'],
+    },
+
+    'sunglassesPositives': {
+            'de': ['sonne'],
+            'ch': ['sonne'],
+            'es': ['sol'],
+            'fr': ['sol'],
+            'uk': ['sun'],
+            'nl': ['zonnen'],
+    },
+
+    'lensesPositives': {
+            'de': ['linse', 'kontakt'],
+            'ch': ['linse', 'kontakt'],
+            'es': ['lentilla', 'lentes', 'contacto'],
+            'fr': ['lentille', 'contact'],
+            'uk': ['contact', 'lense'],
+            'nl': ['kontakt'],
+    }  
 
 }
 
@@ -22,4 +48,3 @@ else:
 
     variables['path'] = r'R:\Marketing\Channels\SEO\Ongoing\Regularly - Weekly reports\python' + '\\'
     variables['os'] = 'windows'
-
